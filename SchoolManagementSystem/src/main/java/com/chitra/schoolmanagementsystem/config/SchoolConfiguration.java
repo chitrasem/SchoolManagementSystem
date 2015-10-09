8f3f389d39d631dff2704943a731c1659b819596
Chitra <sabaychitra84@gmail.com>
2015-10-09 09:58:31 +0700

    f

--- SchoolManagementSystem/src/main/java/com/chitra/schoolmanagementsystem/config/SchoolConfiguration.java
+++ SchoolManagementSystem/src/main/java/com/chitra/schoolmanagementsystem/config/SchoolConfiguration.java
@@ -1,5 +1,40 @@
 package com.chitra.schoolmanagementsystem.config;
 
-public class SchoolConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.chitra.schoolmanagementsystem")
+public class SchoolConfiguration extends WebMvcConfigurerAdapter{
+	
+	@Autowired
+	RoleToUserProfileConverter roleToUserProfileConverter;
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+	
+	/**
+	 * For including css js and images files
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static");
+		
+	}
+	
 
 }
